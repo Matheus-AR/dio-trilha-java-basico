9df80ed938d15692c73814c04d6f4780a9b90151
Matheus de Araújo Rodrigues <69733008+Matheus-AR@users.noreply.github.com>
2024-03-01 14:10:30 -0300

    Condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
-        } else {
+        } else if (nota >= 5) {
+            System.out.println("Recuperação");
+        }else {
             System.out.println("Reprovado");
         }
     }
