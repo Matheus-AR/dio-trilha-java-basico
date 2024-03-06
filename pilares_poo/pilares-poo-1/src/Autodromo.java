4ff83f574d4f3765ba4d1d4dfaf692ebfe0f4cc6
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-03-06 09:15:03 -0300

    Aulas sobre pilares e encapsulamento em poo

---
+++ pilares_poo/pilares-poo-1/src/Autodromo.java
@@ -0,0 +1,10 @@
+public class Autodromo {
+    public static void main(String[] args) throws Exception {
+        Carro jeep = new Carro();
+        jeep.setChassi("897897");
+        jeep.ligar();
+        
+        Moto z400 = new Moto();
+        z400.setChassi("324542");
+    }
+}
