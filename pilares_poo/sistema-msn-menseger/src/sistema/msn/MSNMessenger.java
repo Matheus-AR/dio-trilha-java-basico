a0fe8833e73ee55d97e5739d4d70370338d0b89d
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-03-06 09:58:04 -0300

    Aula sobre abstração em poo

--- pilares_poo/sistema-msn-menseger/src/sistema/msn/MSNMessenger.java
+++ pilares_poo/sistema-msn-menseger/src/sistema/msn/MSNMessenger.java
@@ -3,6 +3,16 @@ package sistema.msn;
 import sistema.ServicoMensagemInstantanea;
 
 public class MSNMessenger extends ServicoMensagemInstantanea {
+
+	@Override
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo MSN Messenger");
+	}
+
+	@Override
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo MSN Messenger");
+	}
 	
 }
 
--- pilares_poo/sistema-msn-menseger/src/sistema/telegram/Telegram.java
+++ pilares_poo/sistema-msn-menseger/src/sistema/telegram/Telegram.java
@@ -3,6 +3,16 @@ package sistema.telegram;
 import sistema.ServicoMensagemInstantanea;
 
 public class Telegram extends ServicoMensagemInstantanea {
+
+	@Override
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo Telegram");
+	}
+
+	@Override
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Telegram");
+	}
 	
 }
 
